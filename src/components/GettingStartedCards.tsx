25b1395104fce4d660cbabac88b8cfe683dce545
Isabella Smallcombe <isabella.smallcombe@gmail.com>
2021-09-08 09:17:35 +1000

    feat: working on getting started and tools cards content"

--- src/components/GettingStartedCards.tsx
+++ src/components/GettingStartedCards.tsx
@@ -2,15 +2,32 @@ import React from 'react'
 import './styles.css'
 
 export const GettingStartedCards = () => {
+  const content = [
+    {
+      docNumber: '01',
+      docTitle: 'Build a NFT auction house in 30 minutes →',
+      docSubtopic: 'GRAPHQL',
+    },
+    {
+      docNumber: '02',
+      docTitle: 'Build a NFT auction house in 30 minutes →',
+      docSubtopic: 'GRAPHQL',
+    },
+    {
+      docNumber: '03',
+      docTitle: 'Build a NFT auction house in 30 minutes →',
+      docSubtopic: 'GRAPHQL',
+    },
+  ]
   return (
     <div className={'getting-started-container'}>
-      <div className={'getting-started-card'}>
-        <text className={'doc-number'}>01</text>
-        <text className={'doc-title'}>
-          Build a NFT auction house in 30 minutes
-        </text>
-        <text className={'doc-subtopic'}>GRAPHQL</text>
-      </div>
+      {content.map((card, index) => (
+        <div key={index} className={'getting-started-card'}>
+          <text className={'doc-number'}>{card.docNumber}</text>
+          <text className={'doc-title'}>{card.docTitle}</text>
+          <text className={'doc-subtopic'}>{card.docSubtopic}</text>
+        </div>
+      ))}
     </div>
   )
 }
--- src/components/ToolsCards.tsx
+++ src/components/ToolsCards.tsx
@@ -2,18 +2,38 @@ import React from 'react'
 import './styles.css'
 
 export const ToolsCards = () => {
+  const content = [
+    {
+      toolsTitle: 'NFT Components',
+      toolsVersion: 'v1.2.3',
+      toolsParagraph:
+        'A tool for developers to simply and reliably interact with the Zora Protocol.',
+    },
+    {
+      toolsTitle: 'NFT Components',
+      toolsVersion: 'v1.2.3',
+      toolsParagraph:
+        'A tool for developers to simply and reliably interact with the Zora Protocol.',
+    },
+    {
+      toolsTitle: 'NFT Components',
+      toolsVersion: 'v1.2.3',
+      toolsParagraph:
+        'A tool for developers to simply and reliably interact with the Zora Protocol.',
+    },
+  ]
+
   return (
     <div className={'getting-started-container'}>
-      <div className={'tools-card'}>
-        <div className={'tools-title-flexbox'}>
-          <text className={'tools-title'}>NFT Components</text>
-          <p className={'tools-version'}>v1.2.3</p>
+      {content.map((card, index) => (
+        <div key={index} className={'tools-card'}>
+          <div className={'tools-title-flexbox'}>
+            <text className={'tools-title'}>{card.toolsTitle}</text>
+            <p className={'tools-version'}>{card.toolsVersion}</p>
+          </div>
+          <text className={'tools-paragraph'}>{card.toolsParagraph}</text>
         </div>
-        <text className={'tools-paragraph'}>
-          A tool for developers to simply and reliably interact with the Zora
-          Protocol.
-        </text>
-      </div>
+      ))}
     </div>
   )
 }
