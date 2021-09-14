f37b4e4a1085aacfbb3ef5dfe61f05aa4acb40f8
Isabella Smallcombe <isabella.smallcombe@gmail.com>
2021-09-14 11:01:56 +1000

    refactor: updating index page and adding static logo images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,10 @@
 import React from 'react'
-import clsx from 'clsx'
 import Layout from '@theme/Layout'
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import styles from './index.module.css'
-import HomepageFeatures from '../components/HomepageFeatures'
-import useThemeContext from '@theme/hooks/useThemeContext'
 import { GettingStartedCards } from '../components/GettingStartedCards'
 import { ToolsCards } from '../components/ToolsCards'
 import { ShaderComponent } from '../components/ShaderComponent'
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext()
-  const { isDarkTheme } = useThemeContext()
-  return (
-    <header
-      className={
-        isDarkTheme
-          ? clsx('hero hero--primary', styles.heroBannerDark)
-          : clsx('hero hero--primary', styles.heroBanner)
-      }
-    >
-      <div className="container">
-        <h1 className={styles.heroTitle}>☼☾</h1>
-        <h2 className={styles.heroSubtitle}>ZORA FOR DEVELOPERS</h2>
-      </div>
-    </header>
-  )
-}
+import ProjectsCards from '../components/ProjectsCards'
 
 export default function Home() {
   return (
@@ -35,17 +13,20 @@ export default function Home() {
       description="Description will go into a meta tag in <head />"
     >
       <ShaderComponent />
-      <HomepageHeader />
+      <div>
+        <h1 className={styles.logoText}>☀☾</h1>
+        <h2 className={styles.headerSubtitle}>ZORA FOR DEVELOPERS</h2>
+      </div>
       <main className={styles.layout}>
         <div className={styles.gettingStartedFlexBox}>
           <p className={styles.gettingStarted}>Getting started</p>
           <p className={styles.viewAll}>VIEW ALL</p>
         </div>
-        
         <GettingStartedCards />
         <p className={styles.tools}>Tools</p>
         <ToolsCards />
         <p className={styles.projects}>Projects using ZORA</p>
+        <ProjectsCards />
       </main>
     </Layout>
   )
