8148a80c7998bd2d6bc66015f04a19041047f627
Isabella Smallcombe <isabella.smallcombe@gmail.com>
2021-09-15 15:44:16 +1000

    refactor: fixing shader to work in deployment

---
+++ src/components/ShaderComponent.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './styles.css'
+import { ethers } from 'ethers'
+import { ShaderFragment } from './ShaderFragment'
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment'
+
+export const ShaderComponent = () => {
+  if (ExecutionEnvironment.canUseDOM) {
+    var GlslCanvas = require('glslCanvas')
+    const canvasRef = React.useRef()
+    React.useEffect(() => {
+      const canvas = canvasRef
+      var sandbox = new GlslCanvas(canvas.current)
+      const rpcProvider = new ethers.providers.JsonRpcProvider(
+        process.env.INFURA,
+      )
+      rpcProvider.getBlockNumber().then((num) => {
+        sandbox.load(ShaderFragment)
+        sandbox.setUniform('u_seed', Math.pow(num, 0.5))
+      })
+
+      canvas.current.style.width = '100%'
+      canvas.current.style.height = '100%'
+    }, [])
+    return <canvas ref={canvasRef} />
+  } else {
+    return <></>
+  }
+}
