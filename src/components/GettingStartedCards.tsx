15fba99c941c3bac6ac1fdd6d94f64387df13288
Isabella Smallcombe <isabella.smallcombe@gmail.com>
2021-09-17 17:52:01 +1000

    chore: fixing file import

--- src/components/GettingStartedCards.tsx
+++ src/components/GettingStartedCards.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './styles.css'
-import { gettingStartedContent } from './content/gettingStartedCardsContent'
+import { gettingStartedContent } from './content/GettingStartedCardsContent'
 
 export const GettingStartedCards = () => {
   return (
