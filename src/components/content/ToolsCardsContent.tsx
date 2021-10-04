23f22279cd912790af2619fe5bf7f282d848adda
Isabella Smallcombe <isabella@ourzora.com>
2021-10-04 23:07:07 +1100

    fix: updating tools cards mock ui

--- src/components/content/ToolsCardsContent.tsx
+++ src/components/content/ToolsCardsContent.tsx
@@ -17,4 +17,40 @@ export const toolsCardsContent = [
     toolsParagraph:
       'A tool for developers to simply and reliably interact with the Zora Protocol.',
   },
-]
+  {
+    toolsTitle: 'NFT Components',
+    toolsVersion: 'v1.2.3',
+    toolsParagraph:
+      'A tool for developers to simply and reliably interact with the Zora Protocol.',
+  },
+  {
+    toolsTitle: 'NFT Components',
+    toolsVersion: 'v1.2.3',
+    toolsParagraph:
+      'A tool for developers to simply and reliably interact with the Zora Protocol.',
+  },
+  {
+    toolsTitle: 'NFT Components',
+    toolsVersion: 'v1.2.3',
+    toolsParagraph:
+      'A tool for developers to simply and reliably interact with the Zora Protocol.',
+  },
+  {
+    toolsTitle: 'NFT Components',
+    toolsVersion: 'v1.2.3',
+    toolsParagraph:
+      'A tool for developers to simply and reliably interact with the Zora Protocol.',
+  },
+  {
+    toolsTitle: 'NFT Components',
+    toolsVersion: 'v1.2.3',
+    toolsParagraph:
+      'A tool for developers to simply and reliably interact with the Zora Protocol.',
+  },
+  {
+    toolsTitle: 'NFT Components',
+    toolsVersion: 'v1.2.3',
+    toolsParagraph:
+      'A tool for developers to simply and reliably interact with the Zora Protocol.',
+  },
+];
