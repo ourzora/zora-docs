2b74d35729b800e9374a1d27981f230fa1523c89
anader123 <alannaderdev@gmail.com>
2022-01-28 16:14:14 -0800

    add v3 overview and small changes

--- src/components/content/ToolsCardsContent.js
+++ src/components/content/ToolsCardsContent.js
@@ -1,41 +1,36 @@
 export const toolsCardsContent = [
   {
-    toolsTitle: 'Auction House',
-    toolsVersion: 'v1.1.3',
+    toolsTitle: 'Asks Module',
+    // toolsVersion: 'v1.1',
     toolsParagraph:
-      'A permissionless reserve price auction protocol that allows anyone to create and run their own curated auction house for any NFT.',
-    toolsLink: '/docs/developer-tools/zdk/auction-house',
+      'Asks is a feature from ZORA V3 that allows anyone to sell an NFT at a specific asking price.',
+    toolsLink: '/docs/smart-contracts/modules/Asks/zora-v3-asks-latest',
   },
   {
     toolsTitle: 'ZDK',
-    toolsVersion: 'v1.2.3',
     toolsParagraph:
       'A tool for developers to simply and reliably interact with the Zora Protocol.',
     toolsLink: '/docs/developer-tools/zdk/introduction',
   },
   {
     toolsTitle: 'Indexer',
-    toolsVersion: 'v1.0.0',
     toolsParagraph:
       'A GraphQL API with indexed data from the Zora protocol and the entire NFT universe.',
     toolsLink: '/docs/developer-tools/indexer/introduction',
   },
   {
     toolsTitle: 'NFT Hooks',
-    toolsVersion: 'v0.5.1',
     toolsParagraph:
       'React Hooks to retrieve pricing and NFT information to display on your own website.',
     toolsLink: '/docs/developer-tools/nft-rendering/nft-hooks/useNft',
   },
   {
     toolsTitle: 'NFT Components',
-    toolsVersion: 'v0.1.12',
     toolsParagraph: 'React components that allow for drop-in rendering of NFTs',
     toolsLink: '/docs/developer-tools/nft-rendering/introduction#nft-components',
   },
   {
     toolsTitle: 'ZORA NFTs',
-    toolsVersion: 'v1.0.7',
     toolsParagraph:
       'An extension of the base ERC-721 NFT Protocol, enabling markets to be owned directly by token creators.',
     toolsLink: '/docs/developer-tools/zdk/zora-nfts',
