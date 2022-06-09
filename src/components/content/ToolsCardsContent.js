59dbdb6fdc32bef487abd3ce57c26b08d836f1f0
anader123 <alannaderdev@gmail.com>
2022-06-08 18:38:34 -0700

    zdk and hooks update

--- src/components/content/ToolsCardsContent.js
+++ src/components/content/ToolsCardsContent.js
@@ -12,26 +12,26 @@ export const toolsCardsContent = [
     toolsLink: '/docs/smart-contracts/zora-v3',
   },
   {
-    toolsTitle: 'Asks Module',
-    toolsParagraph:
-      'Asks is a feature from ZORA V3 that allows anyone to sell an NFT at a specific asking price.',
-    toolsLink: '/docs/smart-contracts/modules/Asks/zora-v3-asks-intro',
-  },
-  {
     toolsTitle: 'NFT Rendering',
     toolsParagraph:
-      'Tools to retrieve pricing and NFT information to display on your own website.',
+      'Easy to use components for displaying any NFT with little to no coding.',
     toolsLink: '/docs/developer-tools/nft-rendering/introduction',
-  },
-  {
-    toolsTitle: 'Editions',
-    toolsParagraph: 'A creator toolkit makes it easy to create an NFT collection.',
-    toolsLink: 'https://create.zora.co/',
   },
   {
     toolsTitle: 'ZDK',
     toolsParagraph:
-      'Docs Coming Soon: A developer kit that makes it easy to use the Zora API without needing to know GraphQL.',
-    toolsLink: 'https://github.com/ourzora/zdk',
+      'A developer kit that makes it easy to use the Zora API without needing to know GraphQL.',
+    toolsLink: '/docs/zora-api/zdk',
+  },
+  {
+    toolsTitle: 'Editions',
+    toolsParagraph: 'An interface that makes it easy to create an NFT collection with no coding requried.',
+    toolsLink: 'https://create.zora.co/',
+  },
+  {
+    toolsTitle: 'NFT Hooks',
+    toolsParagraph:
+      'Tools to quickly retrieve metadata, historical info, and market data for any NFT.',
+    toolsLink: '/docs/developer-tools/nft-hooks/intro',
   },
 ]
